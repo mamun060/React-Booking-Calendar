cf889aa6a98380a7ce6097d91342d78ad6d7c026
mamun060 <obujraju420@gmail.com>
2022-01-11 18:10:43 +0600

    home page add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Galleries from './components/Galleries';
 import RevoCalendar from './components/RevoCalendar';
 import ReactCalandare from './components/ReactCalandare';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
       {/* <Galleries /> */}
       {/* <RevoCalendar /> */}
-      <ReactCalandare />
+      {/* <ReactCalandare /> */}
     </div>
   );
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactCalandare from './components/ReactCalandare'
+
+export default function HomePage() {
+    return (
+        <div>
+            <ReactCalandare />
+        </div>
+    )
+}
